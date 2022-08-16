872bec89f827710d5b93f052bfe31fa278715387
AlexandreKozakAlmeida <alexandre.kozak.almeida@gmail.com>
2022-08-16 01:14:00 -0300

    HTML and JS: Texts update

--- script.js
+++ script.js
@@ -11,19 +11,19 @@ const weight = document.querySelector('#weightInput').value;
 const totalValueCalc = (weight / (height * height)).toFixed(1);
 
   if(totalValueCalc < 18.5) {
-    result.textContent = `${name} , seu peso é ${totalValueCalc}, vocẽ está abaixo do peso.`
+    result.textContent = `${name} , seu IMC é ${totalValueCalc}, vocẽ está abaixo do peso.`
   } else if(totalValueCalc >= 18.5 && totalValueCalc <= 24.9) {
-    result.textContent = `${name} , seu peso é ${totalValueCalc}, vocẽ está com o peso normal.`
+    result.textContent = `${name} , seu IMC é ${totalValueCalc}, vocẽ está com o peso normal.`
   } else if(totalValueCalc >= 25 && totalValueCalc <= 29.9) {
-    result.textContent = `${name} , seu peso é ${totalValueCalc}, vocẽ está com sobrepeso.`
+    result.textContent = `${name} , seu IMC é ${totalValueCalc}, vocẽ está com sobrepeso.`
   } else if(totalValueCalc >= 30 && totalValueCalc <= 34.9) {
-    result.textContent = `${name} , seu peso é ${totalValueCalc}, vocẽ está com obesidade classe 1.`
+    result.textContent = `${name} , seu IMC é ${totalValueCalc}, vocẽ está com obesidade classe 1.`
   } else if(totalValueCalc >=35 && totalValueCalc <= 39.9) {
-    result.textContent = `${name} , seu peso é ${totalValueCalc}, vocẽ está com obesidade classe 2.`
+    result.textContent = `${name} , seu IMC é ${totalValueCalc}, vocẽ está com obesidade classe 2.`
   } else if(totalValueCalc >= 40) {
-    result.textContent = `${name} , seu peso é ${totalValueCalc}, vocẽ está com obesidade classe 3.`
+    result.textContent = `${name} , seu IMC é ${totalValueCalc}, vocẽ está com obesidade classe 3.`
   } else {
-    result.textContent = 'O resultado sairá aqui.'
+    result.textContent = 'Preencha todos os campos e o resultado aparecerá aqui.'
   }
 };
  
